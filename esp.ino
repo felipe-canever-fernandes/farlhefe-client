b70c5231b033e1608a7d658ac838ce3f70a80bea
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2022-07-21 15:23:04 -0300

    Make LED blink

--- esp.ino
+++ esp.ino
@@ -1,10 +1,17 @@
+static auto const led_pin = uint8_t(33);
+
 void setup()
 {
+	pinMode(led_pin, OUTPUT);
 	Serial.begin(115200);
 }
 
 void loop()
 {
-	Serial.println("Hello, world!");
+	static auto led_state = static_cast<bool>(digitalRead(led_pin));
+
+	led_state = !led_state;
+	digitalWrite(led_pin, led_state);
+
 	delay(1000);
 }
